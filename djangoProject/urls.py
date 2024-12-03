a7570afa4ea92971309fada5afd5b0eef7428a24
alexandr <git@github.com:Allexandr78/dj_home_work.git>
2024-12-03 18:59:07 +0100

    hw_lesson21

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,22 +1,27 @@
 """
-URL configuration for djangoProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+URL Configuration
 """
-from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from web_dj.views import home, my_feed, article_detail, add_comment, article_upd, article_del, article_create, topics, \
+    topic_subscribe, topic_unsubscribe, profile, register, set_password, login_user, logout_user, articles_by_date
+
+urlpatterns = {
+    path('', home, name='index'),
+    path('my-feed/', my_feed, name='my-feed'),
+    path('article/<int:article_id>/', article_detail, name='article_detail'),
+    path('<int:article_id>/coment/', add_comment, name='add_comment'),
+    path('<int:article_id>/update/', article_upd, name='article_upd'),
+    path('<int:article_id>/delete/', article_del, name='article_del'),
+    path('create/', article_create, name='article_create'),
+    path('topics/', topics, name='topics'),
+    path('topics/<int:topic_id>/', topic_subscribe, name='topic_subscribe'),
+    path('topics/<int:topic_id>/subscribe/', topic_subscribe, name='topic_subscribe'),
+    path('topics/<int:topic_id>/unsubscribe/', topic_unsubscribe, name='topic_unsubscribe'),
+    path('profile/', profile, name='profile'),
+    path('register/', register, name='register'),
+    path('set-password/', set_password, name='set_password'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
+    path('<int:year>/<int:month>/', articles_by_date, name='articles_by_date'),
+}
--- web_dj/views.py
+++ web_dj/views.py
@@ -1,3 +1,111 @@
-from django.shortcuts import render
+"""
+Модуль предназначен для реализации основных функций обработки данных.
 
-# Create your views here.
+Описание:
+- В этом модуле реализованы функции для чтения, обработки и вывода данных.
+- Модуль нацелен на обработку текстовых файлов и может быть расширен для работы с другими форматами.
+
+Функции:
+- read_data(filepath): функция для чтения данных из файла.
+- process_data(data): функция для обработки данных.
+- output_data(processed_data): функция для вывода обработанных данных.
+
+Дата создания: 01.12.2024
+Версия: 1.0
+"""
+from django.http import HttpRequest, HttpResponse
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    """ Основная страница, на которой будет список всех статей."""
+    return HttpResponse(
+        "Основная страница, на которой будет список всех статей.")
+
+
+def my_feed(request: HttpRequest) -> HttpResponse:
+    """Страница, на которой будут только статьи по темам, на которые подписан пользователь."""
+    return HttpResponse(
+        "Страница, на которой будут только статьи по темам, на которые подписан пользователь.")
+
+
+def article_detail(request: HttpRequest, article_id: int) -> HttpResponse:
+    """Страница, на которой будет отображаться статья по #"""
+    return HttpResponse(
+        f"Cтраница, на которой будет отображаться статья по # {article_id}.")
+
+
+def add_comment(request: HttpRequest, article_id: int) -> HttpResponse:
+    """Адрес, который мы будем использовать для написания комментариев к статье #"""
+    return HttpResponse(
+        f"Адрес, который мы будем использовать для написания комментариев к статье # {article_id}.")
+
+
+def article_upd(request: HttpRequest, article_id: int) -> HttpResponse:
+    """Страница, которую мы будем использовать для изменения существующей статьи #"""
+    return HttpResponse(
+        f"Страница, которую мы будем использовать для изменения существующей статьи # {article_id}.")
+
+
+def article_del(request: HttpRequest, article_id: int) -> HttpResponse:
+    """Адрес, который мы будем использовать для удаления статьи #"""
+    return HttpResponse(
+        f"Адрес, который мы будем использовать для удаления статьи #{article_id} .")
+
+
+def article_create(request: HttpRequest) -> HttpResponse:
+    """Страница, на которой мы будем создавать новые статьи."""
+    return HttpResponse("Страница, на которой мы будем создавать новые статьи.")
+
+
+def topics(request: HttpRequest) -> HttpResponse:
+    """Страница, с перечнем всех тем на сайте."""
+    return HttpResponse("Страница, с перечнем всех тем на сайте.")
+
+
+def topic_articles(request: HttpRequest, topic_id: int) -> HttpResponse:
+    """Страница, со всеми статьями по определенной теме #"""
+    return HttpResponse(f"Страница, со всеми статьями по определенной теме # {topic_id}.")
+
+
+def topic_subscribe(request: HttpRequest, topic_id: int) -> HttpResponse:
+    """Адрес для подписки на конкретную тему #"""
+    return HttpResponse(f"Адрес для подписки на конкретную тему # {topic_id}.")
+
+
+def topic_unsubscribe(request: HttpRequest, topic_id: int) -> HttpResponse:
+    """Адрес для отписки от конкретной темы #"""
+    return HttpResponse(f"Адрес для отписки от конкретной темы #{topic_id} .")
+
+
+def profile(request: HttpRequest) -> HttpResponse:
+    """Страница, с данными пользователя и перечнем его подписок."""
+    return HttpResponse(
+        " Страница, с данными пользователя и перечнем его подписок.")
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    """Страница, регистрацией нового пользователя."""
+    return HttpResponse(" Страница, регистрацией нового пользователя.")
+
+
+def set_password(request: HttpRequest) -> HttpResponse:
+    """Страница, с изменением пароля."""
+    return HttpResponse(" Страница, с изменением пароля.")
+
+
+def login_user(request: HttpRequest) -> HttpResponse:
+    """Страница, для входа на сайт."""
+    return HttpResponse(" Страница, для входа на сайт.")
+
+
+def logout_user(request: HttpRequest) -> HttpResponse:
+    """Адрес для выхода с сайта."""
+    return HttpResponse(" Адрес для выхода с сайта.")
+
+
+def articles_by_date(request: HttpRequest, year: int, month: int) -> HttpResponse:
+    """Страница, на которой будут статьи созданные в конкретный месяц {month} {year}. "
+        "В случае запроса не настоящей даты, должна быть ошибка."""
+    return HttpResponse(
+        f"Страница, на которой будут статьи созданные в конкретный месяц {month} {year}. "
+        "В случае запроса не настоящей даты, должна быть ошибка.")
